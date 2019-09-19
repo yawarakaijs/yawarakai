f550439cabc8c03083dcdaf0ce25a2ae1a750b0c
hanamiyuna <mushiyona10@gmail.com>
2019-09-20 05:35:51 +0800

    Message Updated

--- Bot/message.js
+++ Bot/message.js
@@ -14,11 +14,11 @@ let messagectl = {
     
         // Prefix
     
-        var output = Lang.bot.message.from + ": "
+        var output = `${Lang.bot.message.from}: `
         var outputGroup = Lang.bot.message.group
-        var chatMessage = Lang.bot.message.text + ": " + ctx.message.text
-        var fromChatId = " [ ID:" + ctx.message.from.id + " ]"
-        var groupInfo = " | " + outputGroup + ": [ " + ctx.message.chat.title + " ] ID: " + ctx.message.chat.id
+        var chatMessage = `${Lang.bot.message.text} : ${ctx.message.text}`
+        var fromChatId = ` [ ID: ${ctx.message.from.id} ]`
+        var groupInfo = ` | ${outputGroup} : [ ${ctx.message.chat.title} ] ID: ${ctx.message.chat.id}`
 
         // Case of User had their FIRSTNAME and LAST NAME set
         if((ctx.message.from.first_name && ctx.message.from.last_name)) {
@@ -72,7 +72,7 @@ let messagectl = {
     },
     // Process Context Data
     process: (ctx) => {
-        Message.hears(ctx);
+        Message.hears(ctx)
     },
     // Trigger set for special trigger
     trigger: (text, ctx) => {
@@ -106,7 +106,7 @@ let messagectl = {
                     break
                 case "what2eat":
                     // Send Context to what2eat
-                    what2eat();
+                    what2eat()
                     break
             }
         }
@@ -122,15 +122,93 @@ let messagectl = {
 
 let Message = {
     trigCtl: () => {
-
+        
     },
     hears: (ctx) => {
-        var pattern = /悠月|悠月(酱|喵{1,2}|月{1,2})/gi
-        if(pattern.test(ctx.message.text)) {
-            ctx.reply("在的w")
-            Log.Log.debug("回复至: " + ctx.message.from.id + " - 成功 | 匹配: " + pattern[Symbol.match](ctx.message.text))
+        
+        let loveneko = /^(?!不)(?:喜欢 Neko)|(?:喜欢Neko)/gi
+        let loveyuna = /^(?!不)(?:喜欢佑奈)|(?:喜欢佑奈)/gi
+        let sadmeow = /(?:喵)(?:\.\.\.)+/g
+        let yutsukicute = /(?:悠月)(.*?)(?:可爱)/g
+        let yutsukinotcute = /(.*?)(不)(.*?)(?:悠月)|(?:悠月)(.*?)(不)(.*?)/g
+        let yutsukiyutsukiyu = /(悠月悠月悠)|(悠月悠)/g
+        let yutsukiletsplay = /((?:悠月)|(悠月酱))(?:来玩)/g
+        let hugneeded = /(?:要抱)((抱)|(.*))/gi
+        let tiredforthis = /(^(.*)(?:累))/gi
+        let meowmeow = /(喵~)/gi
+        let dumbyouyue = /(悠月)(()|(是))((?:笨蛋)|(?:讨厌)|(?:大笨蛋))/gi
+        let hugyouyue = /((?:抱抱)|(?:摸摸))(柠檬)/gi
+        var yutsukihi = /悠月|悠月(酱|喵{1,2}|月{1,2})/gi
+
+        // Greetings
+
+        let scmorning = /((.*)|(?:大家))((?:早安安)|(?:早安)|(?:早上好))/gi
+        let yutsukimorning = /((悠月酱)|(?:悠月))((?:早安安)|(?:早安)|(?:早上好)|(?:早))/g
+        let enmorning = /((?:Good Morning))(\w|((.*)(?:.*)))/gi
+
+        let scafternoon = /((.*)|(?:大家))((?:午安安)|(?:午安)|(?:下午好)|(?:中午好))/gi
+        let yutsukiafternoon = /((悠月酱)|(?:悠月))((?:午安)|(?:午安安)|(?:下午好))/g
+        let enafternoon = /((?:Good Afternoon))(\w|((.*)(?:.*)))/gi
+
+        let scnight = /((.*)|(?:大家))((?:晚安安)|(?:晚安))/gi
+        let yutsukinight = /((悠月酱)|(?:悠月))((?:晚安)|(?:晚安安))/g
+        let ennight = /((?:Good Night))(\w|((.*)(?:.*)))/gi
+
+        let scevening = /((.*)|(?:大家))((?:晚上好)|(?:晚好))/gi
+        let yutsukievening = /((悠月酱)|(?:悠月))((?:晚上好)|(?:晚好))/gi
+        let enevening = /((?:Good Evening))(\w|((.*)(?:.*)))/gi
+
+        this.reply(ctx, loveneko, "Neko 知道的话会超开心的！")
+        this.reply(ctx, loveyuna, "佑奈知道的话会超开心的！")
+        this.reply(ctx, sadmeow, "喵...怎么了吗...")
+        this.reply(ctx, /(?:咕噜)/g, "咕噜咕噜咕噜")
+        this.reply(ctx, yutsukinotcute, "喵...悠月做错惹什么嘛...（哭哭")
+        this.reply(ctx, yutsukicute, "啊呜啊呜(捂脸)")
+        this.reply(ctx, yutsukiyutsukiyu, "咕噜咕噜~ 要做什么啦！")
+        this.reply(ctx, yutsukiletsplay, "(竖起耳朵) 悠月也想玩呢，可是佑奈说要继续研究新的东西什么的...弄完这些工作才能玩呢。抱歉啦~")
+        this.reply(ctx, hugneeded, "啊呜啊呜，抱紧紧...（顺毛）")
+        this.reply(ctx, tiredforthis, "揉揉...实在太累的话就休息一下呢喵...")
+        this.reply(ctx, meowmeow, "喵~")
+        this.reply(ctx, dumbyouyue, "呜...悠月对不起呢...有什么对不起你的地方吗......而且...明明是主人的错嘛！")
+        this.reply(ctx, hugyouyue, "喵...扑过去抱住 >_<")
+        this.reply(ctx, yutsukihi, "在的w")
+
+        this.reply(ctx, yutsukimorning, "喵~早安")
+        this.reply(ctx, scmorning, "早安喔")
+        this.reply(ctx, enmorning, "Morning!")
+
+        this.reply(ctx, yutsukiafternoon, "咕噜咕噜，午安安，有闲暇时间的话记得休息放松一下呢~")
+        this.reply(ctx, scafternoon, "已经过去大半天了呢，午安喵")
+        this.reply(ctx, enafternoon, "Good afternoon! Finishing up with all your work?")
+
+        this.reply(ctx, yutsukinight, "嗯喵，晚安。祝你做个好梦呢~")
+        this.reply(ctx, scnight, "晚安喵，好好休息哦")
+        this.reply(ctx, ennight, "Good night! Wish you would have a sweet dream :)")
+
+        this.reply(ctx, yutsukievening, "喵喵，晚上好喔，悠月在研究新奇的东西呢w")
+        this.reply(ctx, scevening, "晚上好，今天过得怎么样呢？")
+        this.reply(ctx, enevening, "Good evening! How's it going today?")
+        
+    },
+    reply(ctx, textPattern, textReply) {
+        if(this.count >= 1) {
+            this.count = 0;
+            return;
         }
-    }
+        else if(this.count == 0) {
+            if(textPattern.test(ctx.message.text)) {
+                this.count ++;
+                ctx.reply(textReply);
+                Log.Log.debug(`回复至: ${ctx.message.from.id} - 成功 | 匹配: ${textPattern[Symbol.match](ctx.message.text)}`);
+                return;
+            }
+            else{
+                return;
+            }
+        }
+        
+    },
+    count: 0
 }
 
 exports.messagectl = messagectl
