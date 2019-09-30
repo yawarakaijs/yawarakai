fae29242152e82905bcb2cfb981fa2094282cd55
Hanami Yuna <github@yutsuki.moe>
2019-10-01 05:44:48 +0800

    Ability to set Webhook on cli

--- Bot/telegram.js
+++ Bot/telegram.js
@@ -1,7 +1,6 @@
 // Dependencies
 
 let Telegraf = require('telegraf')
-//const SocksAgent = require('socks5-https-client/lib/Agent');
 
 // Local Packages
 
@@ -9,12 +8,6 @@ let config = require('../config.json')
 let Log = require('../log')
 let Lang = require('../lang').Lang
 
-/*
-const socksAgent = new SocksAgent({
-    socksHost: config.proxy.host,
-    socksPort: config.proxy.port
-})
-*/
 
 // Creating Bot
 // At this time Single User
@@ -25,27 +18,60 @@ let Ctl = {
 }
 
 let command = (cmd) => {
+    var webhookPort
     var args = cmd.split(" ")
     switch (args[1]) {
         default:
-            console.log(cmd + " " + Lang.bot.telegram.commandInvalid)
+            console.log("Yawarakai: " + cmd + " " + Lang.bot.telegram.commandInvalid)
             break
         case 'h':
         case 'help':
             console.log(Lang.bot.telegram.helpCommand)
             break
         case 'set':
-            Bot.telegram.setWebhook(config.webhook.url)
+            var count, err
+            var num = 2
+            args.forEach(element => {
+                if(element === "--l" || element === "--url" && args[count+1]) {
+                    Bot.telegram.setWebhook(args[count+1])
+                }
+                else if(element === "--p" || element === "--port" && args[count+1]) {
+                    webhookPort = args[count+1]
+                }
+                else if(element === "--l" || element === "--url" && !args[count+1]) {
+                    num = count + 1
+                    err = true
+                }
+                else if(element === "--p" || element === "--port" && !args[count+1]) {
+                    num = count + 1
+                    err = true
+                }
+                else if(args[2]) {
+                    num = count + 1
+                    err = true
+                }
+                else if(config.webhook.url){
+                    console.log("Using default settings")
+                    webhookPort = config.webhook.port != undefined ? config.webhook.port : 8000
+                    Bot.telegram.setWebhook(config.webhook.url)
+                }
+                count++
+            })
+            if(err) {
+                // Display error if  option is invalid
+                console.log(num)
+                console.log(Lang.bot.telegram.commandParameterMissing[0] + args[num] + Lang.bot.telegram.commandParameterMissing[1])
+                Log.AnonymousLog.trace( Lang.app.commandExecution + ": " + cmd)
+            }      
             break
         case 'start':
             Log.Log.debug("Telegram Bot: " + config.botname + Lang.app.starting)
-            Bot.startWebhook('/', null, config.webhook.port)
+            Bot.startWebhook('/', null, webhookPort != undefined ? webhookPort : 8000)
             break
         case 'stop':
-            Bot.stop(() => Log.Log.info("Shutting down..."))
-            return
+            Bot.stop()
+            break;
     }
-    return
 }
 
 exports.command = command
